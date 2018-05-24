fbbb9824d2824e4aeb3f40a682ddf444d6064c04
schomy <sebastien.chomy@gmail.com>
2018-05-24 23:06:32 +0200

    #5 Jeu de données de test
    - Add doctrine/doctrine-fixtures-bundle
    - Fixtures Task ans User Entity

---
+++ src/AppBundle/DataFixtures/ORM/UserFixtures.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * This file is part of oc_todolist project
+ *
+ * @author: Sébastien CHOMY <sebastien.chomy@gmail.com>
+ * @since 2018/05
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+
+/**
+ * Class UserFixtures
+ *
+ * @package AppBundle\DataFixtures\ORM
+ */
+class UserFixtures extends Fixture implements ContainerAwareInterface
+{
+    /** *******************************
+     *  PROPERTIES
+     */
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var UserPasswordEncoder
+     */
+    protected $encoder;
+
+    /** *******************************
+     *  METHODS
+     */
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            $user = new User();
+            $user->setUsername("username_$i");
+            $user->setEmail("username_$i@mail.com");
+            $user->setPassword($this->encoder->encodePassword($user, '12345'));
+
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+
+    /**
+     * @param ContainerInterface|null $container
+     *
+     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException
+     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+        $this->encoder = $this->container->get('security.password_encoder');
+    }
+}
